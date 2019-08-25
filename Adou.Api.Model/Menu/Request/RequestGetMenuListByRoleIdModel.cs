e4e99fb7b4069ef511672f44cefe2c08590a1896
adou <doufu_0329@163.com>
2019-08-26 00:30:32 +0800

    发布

---
+++ Adou.Api.Model/Menu/Request/RequestGetMenuListByRoleIdModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adou.Api.Model
+{
+    public class RequestGetMenuListByRoleIdModel : RequestBaseModel
+    {
+        /// <summary>
+        /// 角色编号
+        /// </summary>
+        public long RoleId { get; set; }
+    }
+}
---
+++ Adou.Api.Service/AdServices/Menu/GetMenuListByRoleIdService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Adou.Api.Model;
+using Adou.Repositories.PetaPoco.Adou;
+using Adou.Entity.PetaPocoModels.AdouModel;
+
+namespace Adou.Api.Service.AdServices.Menu
+{
+    public class GetMenuListByRoleIdService : ApiBase<RequestGetMenuListByRoleIdModel>
+    {
+        public MenuRep menuRep { get; set; }
+        /// <summary>
+        /// 执行业务逻辑
+        /// </summary>
+        protected override void ExecuteMethod()
+        {
+            var result = menuRep.GetMenuListByRoleId(this.Parameter.RoleId);
+
+            this.Result.Data = result;
+        }
+    }
+}
